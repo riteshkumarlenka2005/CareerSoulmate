f81b41b81f29ffe7e73f19ae540af9f9a7072eb7
Ritesh Kumar Lenka <lenkariteshkumar2005@gmail.com>
2026-04-05 17:01:40 +0530

    chore: update environment configurations and database connections for production

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/careersoulmate');
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://lenkariteshkumar2005:B6NkQkTGJ1exEE4i@clustercareersoulmate.jqxh8yt.mongodb.net/careersoulmate?appName=ClusterCareerSoulmate');
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB connection error: ${error.message}`);
